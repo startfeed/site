a12ba17be4b836e9045f53d49fcfdf57fc282767
Paul Credoz <paul.credoz@epita.fr>
2022-09-15 14:51:06 +0200

    add link and JS copy

---
+++ js/copyMail.js
@@ -0,0 +1,9 @@
+function myFunction() {
+
+  // Select the text field
+    v = "contact@startfeed.fr"
+    navigator.clipboard.writeText(v);
+  
+  // Alert the copied text
+    alert("Mail copié : " + v);
+}
